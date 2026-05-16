d10deddba101c666e151615eaf78ba64b97d88ae
Kollin <kollin@hey.com>
2026-05-15 23:06:25 -0500

    Disjoint player display and collision

--- src/main.c
+++ src/main.c
@@ -5,8 +5,28 @@
 #define DEFAULT_WIDTH 800
 #define DEFAULT_HEIGHT 480
 #define DEFAULT_FONT_SIZE 64
+#define DEFAULT_PLAYER_SPEED 8
 
-// Audio callback to generate a sine wave in real-time
+typedef struct Player {
+    Rectangle collision;
+    Rectangle display;
+    float rotation;
+    Color color;
+} Player;
+
+void initPlayer(Player *player, int x, int y, int width, int height,
+                float rotation, Color color) {
+    // Player is initialized with some initial position. Its display rect is
+    // offset from its collision rect by 1/2 its width and height to match what
+    // is rendered by DrawRectanglePro
+    *player =
+        (Player){.collision = (Rectangle){x - (width / 2), y - (height / 2),
+                                          width, height},
+                 .display = (Rectangle){x, y, width, height},
+                 .rotation = rotation,
+                 .color = color};
+}
+
 void DumbAlarmCallback(void *bufferData, unsigned int frames) {
     float *buffer = (float *)bufferData;
     static float phase = 0.0f;
@@ -45,19 +65,70 @@ void f_ToggleFullscreen() {
     }
 }
 
-void updatePlayerPos(Rectangle *player) {
+int clamp(int min, int max, int value) {
+    if (value < min) {
+        return min;
+    } else if (value > max) {
+        return max;
+    }
+    return value;
+}
+
+void updatePlayerPos(Player *player) {
+    // Because collision and display are now disjointed, they are subject to
+    // different bounds; display is to the bottom-right of the collision square,
+    // so we have to make sure to allow it to exceed the bounds of the screen a
+    // bit to stay in sync
+    int display_min_x = ((*player).collision.width / 2);
+    int display_min_y = ((*player).collision.width / 2);
+    int display_max_x = GetScreenWidth() - (0.5 * (*player).display.width);
+    int display_max_y = GetScreenHeight() - (0.5 * (*player).display.height);
+
+    // NOTE: Only works because width and height are divisible by 2. Otherwise,
+    // we're one pixel off.
+    int collision_min_x = 0;
+    int collision_min_y = 0;
+    int collision_max_x = display_max_x - ((*player).collision.width / 2);
+    int collision_max_y = display_max_y - ((*player).collision.height / 2);
+
     if (IsKeyDown(KEY_W)) {
-        (*player).y -= 5;
+        (*player).collision.y =
+            clamp(collision_min_y, collision_max_y,
+                  (*player).collision.y - DEFAULT_PLAYER_SPEED);
+        (*player).display.y = clamp(display_min_y, display_max_y,
+                                    (*player).display.y - DEFAULT_PLAYER_SPEED);
     };
     if (IsKeyDown(KEY_A)) {
-        (*player).x -= 5;
+        (*player).collision.x =
+            clamp(collision_min_x, collision_max_x,
+                  (*player).collision.x - DEFAULT_PLAYER_SPEED);
+        (*player).display.x = clamp(display_min_x, display_max_x,
+                                    (*player).display.x - DEFAULT_PLAYER_SPEED);
     };
     if (IsKeyDown(KEY_S)) {
-        (*player).y += 5;
+        (*player).collision.y =
+            clamp(collision_min_y, collision_max_y,
+                  (*player).collision.y + DEFAULT_PLAYER_SPEED);
+        (*player).display.y = clamp(display_min_y, display_max_y,
+                                    (*player).display.y + DEFAULT_PLAYER_SPEED);
     };
     if (IsKeyDown(KEY_D)) {
-        (*player).x += 5;
+        (*player).collision.x =
+            clamp(collision_min_x, collision_max_x,
+                  (*player).collision.x + DEFAULT_PLAYER_SPEED);
+        (*player).display.x = clamp(display_min_x, display_max_x,
+                                    (*player).display.x + DEFAULT_PLAYER_SPEED);
     };
+}
+
+void updatePlayerRot(float *rotation) {
+    if (IsKeyPressed(KEY_SPACE)) {
+        if (IsKeyDown(KEY_LEFT_SHIFT)) {
+            *rotation -= 45.0f;
+        } else {
+            *rotation += 45.0f;
+        }
+    }
 }
 
 void showStatusMessage(bool isColliding) {
@@ -96,8 +167,11 @@ int main(void) {
 
     SetTargetFPS(60);
 
-    Rectangle player = {(float)DEFAULT_WIDTH / 2, (float)DEFAULT_HEIGHT / 2, 50,
-                        50};
+    Player player;
+    initPlayer(&player, (float)DEFAULT_WIDTH / 2, (float)DEFAULT_HEIGHT / 2, 50,
+               50, 0.0f, BLUE);
+    // Rectangle player = {(float)DEFAULT_WIDTH / 2, (float)DEFAULT_HEIGHT / 2,
+    // 50, 50};
     Rectangle enemy = {(float)DEFAULT_WIDTH * 0.85f,
                        (float)DEFAULT_HEIGHT * 0.75f, 150, 150};
 
@@ -115,16 +189,22 @@ int main(void) {
             f_ToggleFullscreen();
         }
 
-        updatePlayerPos(&player);
-        DrawRectangleRec(player, BLUE);
-        DrawRectangleRec(enemy, RED);
-
-        // Collision handling
-
         DrawText(warningMessage, enemy.x + (MeasureText(warningMessage, 8) / 4),
                  enemy.y - 10, 8, RED);
 
-        areColliding = CheckCollisionRecs(player, enemy);
+        updatePlayerPos(&player);
+        updatePlayerRot(&player.rotation);
+
+        DrawRectangleRec(enemy, RED);
+        // To rotate about its center, the Rect is shifted, so its positioning
+        // logic is messed up. Need a solution to this.
+        DrawRectanglePro(player.display,
+                         (Vector2){player.display.width / 2.0f,
+                                   player.display.height / 2.0f},
+                         player.rotation, player.color);
+
+        // Collision handling
+        areColliding = CheckCollisionRecs(player.collision, enemy);
         showStatusMessage(areColliding);
 
         if (areColliding) {
